d4dda856e6fb5bc9f199f4cf69da2b3c27e6e764
royandri <royandri502@gmail.com>
2020-01-15 22:41:50 +0700

    Added searching to user profile API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response 
 from rest_framework import status
-from profiles_api import serializers
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+
+from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 
 class HelloApiView(APIView):
     # Test API View
@@ -101,3 +106,12 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         # Handle removing an object
         return Response({'http_method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    # Handle crating and updating profiles
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
