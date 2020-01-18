599f2bbadbb81750ec856109b8660bc2f4cdcf41
royandri <royandri502@gmail.com>
2020-01-18 17:20:06 +0700

    Added user feed API

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -19,13 +19,21 @@ class UserProfileSerializer(serializers.ModelSerializer):
             } 
         }
 
-    def cerate(self, validated_data):
+    def create(self, validated_data):
         # Create and return a new user
         user = models.UserProfile.objects.create_user(
             email=validated_data['email'],
             name=validated_data['name'],
-            password=validated_data['password ']
+            password=validated_data['password']
         )
 
         return user
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    #  Serializers profilr feed item
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwargs = {'user_profile': {'read_only': True}}
      
